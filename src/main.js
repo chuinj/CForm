ec957159e22c7118e4c530f60b269b4002933376
chuin <738587007@qq.com>
2023-04-21 22:18:48 +0800

    全局引入element-ui

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Button } from 'element-ui';
+import ElementUI from 'element-ui';
 import './styles/element-variables.scss'
 import CForm from './packages/index'
 import CCrud from './packages/index'
 import './components/automatic/index'
 
-Vue.use(Button)
+Vue.use(ElementUI)
+
 Vue.use(CForm)
 Vue.use(CCrud)
 
